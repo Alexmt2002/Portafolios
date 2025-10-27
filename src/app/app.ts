de9c758f6789c7bf2c70c31e6afad620213c10ee
Alexmt2002 <alemolinatic@gmail.com>
2025-10-28 00:56:50 +0100

    Arreglado el centrado de la presentación, la ruta de la imagen y creado el componente presentación

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { Header } from "./component/header/header";
 import { Card } from './component/card/card';
+import { Presentacion } from './component/presentacion/presentacion';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatIconModule, Header, Card],
+  imports: [MatToolbarModule, MatIconModule, Header, Card, Presentacion],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
